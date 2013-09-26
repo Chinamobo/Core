7c53732a029f65adfa92db4df85a71678216952c
XiaGuanghua <xiaguanghua@chinamobo.com>
2013-09-26 21:15:46 +0800

    tmp commit

--- RFKit/category/Foundation/NSArray+RFKit.m
+++ RFKit/category/Foundation/NSArray+RFKit.m
@@ -24,4 +24,31 @@
     va_end(ap);
 }
 
+- (NSMutableArray *)dividedBySize:(NSInteger)size {
+    int category_count = [self count];
+    int arr_count;
+    int mod = category_count % size;
+    int left;
+    if (category_count >0) {
+        if (mod == 0) {
+            arr_count = category_count/size;
+            left = size;
+        }else{
+            arr_count = category_count/size + 1;
+            left = mod;
+        }
+        NSMutableArray *arraySet = [NSMutableArray arrayWithObjects:nil];
+        for (int i = 0; i<arr_count; i++) {
+            NSMutableArray *each = [NSMutableArray arrayWithObjects:nil];
+            for (int j = i*size; j<(i+1==arr_count?(left+i*size):(i+1)*size); j++) {
+                [each addObject:[self objectAtIndex:j]];
+            }
+            [arraySet addObject:each];
+            
+        }
+        return arraySet;
+    }
+    return nil;
+}
+
 @end
--- RFKit/category/Foundation/NSDate+RFKit.m
+++ RFKit/category/Foundation/NSDate+RFKit.m
@@ -20,19 +20,13 @@
 }
 
 - (BOOL)isSameDayWithDate:(NSDate *)date {
-    if (!date) return false;
-
     NSDateComponents *target = [[NSCalendar currentCalendar] components:(NSYearCalendarUnit | NSMonthCalendarUnit | NSDayCalendarUnit) fromDate:date];
     NSDateComponents *source = [[NSCalendar currentCalendar] components:(NSYearCalendarUnit | NSMonthCalendarUnit | NSDayCalendarUnit) fromDate:self];
     return [target isEqual:source];
 }
 
 //! ref: http://stackoverflow.com/a/4739650/945906
-+ (NSInteger)daysBetweenDate:(NSDate *)fromDateTime andDate:(NSDate *)toDateTime {
-    if (!fromDateTime && !toDateTime) return 0;
-    NSParameterAssert(fromDateTime != nil);
-    NSParameterAssert(toDateTime != nil);
-    
++ (NSInteger)daysBetweenDate:(NSDate*)fromDateTime andDate:(NSDate*)toDateTime {
     NSDate *fromDate;
     NSDate *toDate;
     
